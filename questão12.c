bb0c8a6d1873d4a3da9b86e2cae8511ddd28ad37
Ranier Sales <145566289+RanierSales@users.noreply.github.com>
2024-03-20 11:30:17 -0400

    Add files via upload

---
+++ questão12.c
@@ -0,0 +1,21 @@
+/* 
+
+Questão 12: Elabore um programa que contenha uma constante qualquer do tipo int. Use o
+comando const. Imprima essa constante.
+
+
+Alunos: Ranier Sales e Matheus Garcia
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+const int numero = 24; //constante inteira
+
+int main(void) {
+
+  printf("%d",numero); //print da constante
+
+  system("pause");
+  return 0;
+}
