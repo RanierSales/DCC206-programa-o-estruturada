da50b843988f66bc398f489a62dc5fe8d72d3347
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-20 19:06:37 -0300

    Create questão8.c

---
+++ questão8.c
@@ -0,0 +1,13 @@
+/*8. Faça um programa que leia dois números inteiros e depois os imprima na ordem inversa em que eles foram lidos.*/ 
+
+#include <stdio.h> 
+
+int main(void) { //paradigma vazio para não precisar usar return  
+  int num1;
+  int num2;
+  printf("Digite um número : "); 
+  scanf("%d",&num1);  
+  printf("Digite outro número : "); 
+  scanf("%d",&num2); 
+  printf("Os números digitados na ordem inversa foi %d e %d",num2,num1);
+}
