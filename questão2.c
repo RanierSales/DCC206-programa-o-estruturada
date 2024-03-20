0ab0fb5e9a2027a7f6868cfc59301ee24a7d04e4
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-20 19:05:26 -0300

    Create questão2.c

---
+++ questão2.c
@@ -0,0 +1,11 @@
+/*2. Escreva um programa que leia um número inteiro e depois o imprima.
+*/
+#include <stdio.h>  
+int main(void) { //main function e paradigma vazio para não precisar usar return 0;  
+  int num;
+  printf("Digite um número inteiro: "); 
+  scanf("%d",&num);
+  printf("O numero digitado foi %d", num);  
+} 
+
+//Vai dar erro no programa por conta de colocar um tipo de variavel diferente no segundo printf
