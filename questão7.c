dda5abcd81a02511e2a367999f72e35e597437fb
Ranier Sales <145566289+RanierSales@users.noreply.github.com>
2024-03-20 11:00:41 -0400

    Add files via upload

---
+++ questão7.c
@@ -0,0 +1,26 @@
+/* 
+
+Questão 7: Elabore um programa que leia um caractere e depois o imprima como um valor inteiro.
+
+Alunos: Ranier Sales e Matheus Garcia
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  char caractere; //criação de variável para recebimento do caractere
+
+  printf("digite um caractere qualquer: ");
+  scanf("%c", &caractere); //recebimento do caractere
+
+  int valorDoCaractere = caractere; //transformação do caractere em valor inteiro
+
+  printf("o valor lido é: %d", valorDoCaractere); //print do valor inteiro do caractere
+
+  system("pause");
+  return 0;
+}
+
