6c0c5523e6ffd3ce0e08093f9d443095d7df42a5
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-20 19:04:54 -0300

    Create questão1.c

---
+++ questão1.c
@@ -0,0 +1,10 @@
+/*1. Elabore um programa que escreva as mensagens “Início do programa” e “Fim” na tela, uma em cada linha, usando apenas um comando printf().*/
+
+#include <stdio.h> 
+
+
+int main(void) { 
+  printf("Inicio do Programa\n"); 
+  printf("Fim do Programa\n");  
+}
+ //main function e paradigma vazio para na retornar nada;
