b861a402aab87adeeae557bdf814188b553ffe01
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-20 19:06:58 -0300

    Create questão10.c

---
+++ questão10.c
@@ -0,0 +1,13 @@
+/*10. Elabore um programa que solicite ao usuário entrar com o valor do dia, mês e ano
+(inteiros). Em seguida, imprima os valores lidos separados por uma barra (\).
+*/ 
+#include <stdio.h>
+int main(void){ //funtion void (sem retorno).
+  //variaveis com as informações do dia, mês e ano.
+  int day;
+  int month; 
+  int year;
+  printf("Digite o dia, mês e ano que você nasceu:\n"); 
+  scanf("%d/%d/%d",&day,&month,&year); //usamos "/" para separar o input 
+  printf("A data é %d/%d/%d", day,month,year);
+}
