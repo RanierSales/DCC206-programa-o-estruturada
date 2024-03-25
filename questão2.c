fed2f4afbfe830095e9bdc5fc3d5d55862870a9e
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-25 11:11:20 -0300

    Update questão2.c

--- questão2.c
+++ questão2.c
@@ -1,5 +1,6 @@
-/*2. Escreva um programa que leia um número inteiro e depois o imprima.
-*/
+/*2. Escreva um programa que leia um número inteiro e depois o imprima.*/ 
+
+
 #include <stdio.h>  
 int main(void) { //main function e paradigma vazio para não precisar usar return 0;  
   int num;
