7717ab39da41f5cce26d95cbd133e00c6f3c8247
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-25 11:10:47 -0300

    Update questão2.c

--- questão2.c
+++ questão2.c
@@ -8,4 +8,7 @@ int main(void) { //main function e paradigma vazio para não precisar usar retur
   printf("O numero digitado foi %d", num);  
 } 
 
-//Vai dar erro no programa por conta de colocar um tipo de variavel diferente no segundo printf
+//Vai dar erro no programa por conta de colocar um tipo de variavel diferente no segundo printf 
+
+//Matheus Garcia 2023010191 
+//Ranier Sales 2023011223
