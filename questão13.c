dd9f4494425201107b20ec55dcb74ab986ca72d0
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-25 11:10:19 -0300

    Update questão13.c

--- questão13.c
+++ questão13.c
@@ -4,7 +4,9 @@ Questão 13: Faça um programa que leia um caractere do tipo char e depois o imp
 aspas duplas. Assim, se o caractere lido for a letra A, deverá ser impresso “A”.
 
 
-Alunos: Ranier Sales e Matheus Garcia
+Alunos: 
+Matheus Garcia 2023010191 
+Ranier Sales 2023011223
 
 */
 
