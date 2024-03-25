9fe3dafe2a65527679e508e06a12855deb9472e0
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-03-25 11:14:01 -0300

    Update questçao14.c

--- questçao14.c
+++ questçao14.c
@@ -1,4 +1,6 @@
 /*14. Faça um programa que leia três caracteres do tipo char e depois os imprima um em cada linha. Use um único comando printf() para isso.*/ 
+
+
 #include <stdio.h> 
 int main(void) { //paradigma vazio para não precisar usar return 0;  
   char letra1;
@@ -8,4 +10,7 @@ int main(void) { //paradigma vazio para não precisar usar return 0;
   scanf("%c %c %c",&letra1,&letra2,&letra3); 
   printf("Os caracteres digitados foram:\n%c\n%c\n%c",letra1,letra2,letra3);
 }  
+
+//Matheus Garcia 2023010191 
+//Ranier Sales 2023011223
   
